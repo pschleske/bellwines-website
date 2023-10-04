bed20d8313093c94e37248514fa2cbb875d84de1
Priscila Schleske <plasch7@gmail.com>
2023-10-04 15:51:07 -0600

    styles payment success page

--- src/Pages/PaymentSuccess.jsx
+++ src/Pages/PaymentSuccess.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { Flex, Container, IconButton, Text } from '@chakra-ui/react';
+import { CheckIcon } from '@chakra-ui/icons';
 
 export const PaymentSuccess = () => {
     return (
-        <div>
-            <h1>Your payment was successful!</h1>
-            <h3>You'll receive an email confirmation shortly!</h3>
-        </div>
+        <Flex justifyContent='center' justify='center' alignItems='center' marginTop='5vh' >
+            <Container border='1px solid' borderColor='green.500' borderRadius='10px' boxShadow='lg' height={500} width={800} >
+                <Container>
+                    <br />
+                    <br />
+                    <Flex justifyContent='center'>
+                        <IconButton isRound colorScheme='green' size='lg' icon={<CheckIcon />} />
+                    </Flex>
+                    <br />
+                    <br />
+                    <Text align='center' fontSize='4xl' color='green'> Thank you! </Text>
+                    <br />
+                    <Text align='center' fontSize='4xl' color='green'> Your payment was successful.</Text>
+                    <br />
+                    <Text align='center' fontSize='2xl' color=''>You'll receive an email confirmation shortly!</Text>
+                </Container>
+            </Container>
+        </Flex >
     )
 }
