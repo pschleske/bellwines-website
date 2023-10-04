e5290315d6a09c027fb3e19629e264a9de1295f7
Priscila Schleske <plasch7@gmail.com>
2023-10-04 13:00:54 -0600

    fixes width and height of calendar

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Calendar = () => {
     return (
-        <iframe src="https://sharing.clickup.com/10642757/c/h/a4ta5-3947/f606048c0a935a9" frameBorder="10" height={800} width={1500}></iframe>
+        <iframe src="https://sharing.clickup.com/10642757/c/h/a4ta5-3947/f606048c0a935a9" frameBorder="10" height='1000px' width='100%'></iframe>
     )
 }
